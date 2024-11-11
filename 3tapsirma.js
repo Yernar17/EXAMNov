8fb3d0d46dca00cd62f408765922fdc47c5dd282
Yernar Mynzhasar <ernarmynzhasar8@gmail.com>
2024-11-11 18:13:55 +0500

    Commit push

--- 3tapsirma.js
+++ 3tapsirma.js
@@ -7,4 +7,6 @@ if (a1 > b1) {
 } else if (a1 < b1) {
     alert("a sany b sanynan kishi");
     alert("a jane b sandarynyn ortashasy" + (b1 + a1) /2 );
+} else if(a1 = b1) {
+    alert("a jane b sandary ten")
 }
